59b64bfc3fe5eed5b701b6d7c13fecbe2a107b00
ntsly <ngothisaoly@gmail.com>
2021-04-03 08:49:51 +0700

    Add more way to create uni

--- src/main/java/congnghetuts/com/quarkus/firstProgram/MutinyFirstProgram.java
+++ src/main/java/congnghetuts/com/quarkus/firstProgram/MutinyFirstProgram.java
@@ -2,9 +2,12 @@ package congnghetuts.com.quarkus.firstProgram;
 
 import io.smallrye.mutiny.Uni;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
 public class MutinyFirstProgram {
     public static void main(String[] args) {
-        Uni.createFrom().item("Cong nghe tuts xin chao")
+       Uni.createFrom().item("Cong nghe tuts xin chao")
                 .onItem().transform(item -> item + " mutiny")
                 .onItem().transform(String::toUpperCase)
                 .subscribe().with(
@@ -25,6 +28,46 @@ public class MutinyFirstProgram {
                 uni.subscribe().with(
                 item -> System.out.println(">> " + item));
 
+        Uni<String> uniFailed = Uni.createFrom().failure(new Exception("Have a exception"));
+        uniFailed.subscribe().with(item -> System.out.println(item),
+                f -> System.out.println("Fail with error: " + f)
+                );
 
+        Uni<Void> uniNull = Uni.createFrom().nullItem();
+        uniNull.subscribe().with(item -> System.out.println("Data: " + item),
+                f -> System.out.println("Fail with error: " + f)
+        );
+
+
+        Uni<String> uniEmitter = Uni.createFrom().emitter(e->
+        {
+            String result = "This is result";
+            e.complete(result);
+        });
+
+        uniEmitter.subscribe().with(item -> System.out.println("Data: " + item),
+                f -> System.out.println("Fail with error: " + f)
+        );
+
+
+
+        System.out.println("\n------------Creating Unis from a CompletionStage: ");
+        CompletionStage cf =
+                CompletableFuture.runAsync(() ->  performTask("first stage"));
+
+        Uni uniCompletionStage = Uni.createFrom().completionStage(cf);
+        CompletableFuture tt = uniCompletionStage.subscribe().asCompletionStage();
+        tt.join();
+        System.out.println("\n------------After creating Unis from a CompletionStage: ");
+    }
+
+    private static String performTask(String stage) {
+        System.out.println("stage: " + stage +  ", thread: " + Thread.currentThread().getName());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return stage + " test";
     }
 }
